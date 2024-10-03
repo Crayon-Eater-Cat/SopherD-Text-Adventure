e70a8194a9f352b10663b6f1c90f4129d3f975f0
MUKILTEO\1717479 <1717479@mukilteo.wednet.edu>
2024-10-03 09:58:52 -0700

    Brainstorming

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string carry;
+            string name;
+            string output = "";
+            Console.WriteLine("Salutations, new user. Thou art named...");
+            name = Console.ReadLine();
+            Console.WriteLine("Welcome, " + name + ", we've much to discuss.");
+            carry = Console.ReadLine();
+            Console.WriteLine("*A few seconds later, your eyes become flooded with light.\n" +
+                "A warm sensation overcomes you for a moment, like you are standing in the sun with a cool breeze*\n");
+            Console.ReadLine();
         }
     }
 }
